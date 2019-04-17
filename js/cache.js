0a6de262c9d5076016f1eee9ebea4af50264c0d8
Carlos Coloma <ccescribano@gmail.com>
2019-04-17 15:11:03 +0100

    Update gh-pages

--- js/cache.js
+++ js/cache.js
@@ -2,18 +2,10 @@ if (fetch) {
   var key = "cacheClean";
   if (!localStorage.getItem(key)) {
     fetch("http://commit-conf.com", {
-      headers: {
-        Accept:
-          "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
-      },
       cache: "reload",
       mode: "no-cors"
     });
     fetch("https://commit-conf.com", {
-      headers: {
-        Accept:
-          "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
-      },
       cache: "reload",
       mode: "no-cors"
     });
